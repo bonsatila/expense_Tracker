241d09d85aef0a4c5bbc98339e678d524df1b622
Bonsa-Tilahun <bonaatilahun@gmail.com>
2020-08-20 03:24:21 -0600

    platform independance completed

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -93,6 +95,17 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
+                    Platform.isIOS? 
+                    CupertinoButton(
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: _presentDatePicker,
+                    )
+                    :
                     FlatButton(
                       textColor: Theme.of(context).primaryColor,
                       child: Text(
