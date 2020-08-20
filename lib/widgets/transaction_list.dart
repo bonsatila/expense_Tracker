1f01b19d9ac8d17ff5e625719a7bf095962ea646
Bonsa-Tilahun <bonaatilahun@gmail.com>
2020-08-20 02:31:57 -0600

    utilizing device size properly

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -58,7 +58,15 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
+                  trailing: MediaQuery.of(context).size.width > 360 ?
+                  FlatButton.icon(
+                    onPressed: () => deleteTx(transactions[index].id), 
+                    icon: Icon(Icons.delete), 
+                    label: Text('Delete'),
+                    textColor: Theme.of(context).errorColor,
+                  )
+                  : 
+                  IconButton(
                     icon: Icon(Icons.delete),
                     color: Theme.of(context).errorColor,
                     onPressed: () => deleteTx(transactions[index].id),
