ca7e60b26028c8ae6c4f60cd72ddb30a9f4c5c20
Bonsa-Tilahun <bonaatilahun@gmail.com>
2020-08-20 01:10:27 -0600

    reset widgets

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,37 +1,48 @@
-import 'package:expense_tracker/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:expense_tracker/models/transaction.dart';
+
+import './chart_bar.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
+
   Chart(this.recentTransactions);
 
-  List<Map<String, Object>> get groupedTransactionValues{
-    return List.generate(7, (index)  {
-      final weekDay = DateTime.now().subtract(Duration(days: index));
-      double totalSum = 0.0;
-      for(var i=0; i < recentTransactions.length; i++){
-        if(recentTransactions[i].date.day == weekDay.day && 
-        recentTransactions[i].date.month == weekDay.month && 
-        recentTransactions[i].date.year == weekDay.year){
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
         }
       }
+
       return {
-        'day': DateFormat.E().format(weekDay).substring(0,1), 'amount': totalSum
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum,
       };
-    },).reversed.toList();
+    }).reversed.toList();
   }
-  double get totalSpending{
-    return groupedTransactionValues.fold(0.0, (previousValue, element) => previousValue+element['amount']);
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(5),
-      child: Container(
+      margin: EdgeInsets.all(20),
+      child: Padding(
         padding: EdgeInsets.all(10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -39,14 +50,16 @@ class Chart extends StatelessWidget {
             return Flexible(
               fit: FlexFit.tight,
               child: ChartBar(
-                data['day'], 
-                data['amount'], 
-                totalSpending == 0.0 ? 0.0 : (data['amount'] as double)/totalSpending
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
               ),
-            ); 
+            );
           }).toList(),
         ),
-      )
+      ),
     );
   }
 }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,105 +2,113 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
-  Function addTransaction;
-  NewTransaction(this.addTransaction);
+  final Function addTx;
+
+  NewTransaction(this.addTx);
 
   @override
   _NewTransactionState createState() => _NewTransactionState();
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final amountController = TextEditingController();
-  DateTime _selecteDate;
-
-  void _submitData(){
-    if(amountController.text.isEmpty) return;
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
-    if(enteredTitle.isEmpty || enteredAmount <=0 || _selecteDate == null){
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
       return;
     }
-    widget.addTransaction(enteredTitle, enteredAmount, _selecteDate);
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
+
     Navigator.of(context).pop();
   }
 
-  void _presentDatePicker(){
+  void _presentDatePicker() {
     showDatePicker(
-      context: context, 
-      initialDate: DateTime.now(), 
-      firstDate: DateTime(2020), 
-      lastDate: DateTime.now()
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
     ).then((pickedDate) {
-      if(pickedDate == null){
+      if (pickedDate == null) {
         return;
       }
       setState(() {
-        _selecteDate = pickedDate;
+        _selectedDate = pickedDate;
       });
     });
+    print('...');
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child:Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (val) => amountInput = val,
+            ),
+            Container(
+              height: 70,
+              child: Row(
                 children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title'
-                    ),
-                    // onChanged: (value) => {
-                    //   titleInput = value
-                    // },
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Amount'
-                    ),
-                    // onChanged: (value) => {
-                    //   amountInput = value
-                    // },
-                    controller: amountController,
-                    keyboardType: TextInputType.numberWithOptions(decimal:true),
-                    onSubmitted: (_) => _submitData() ,
-                  ),
-                  Container(
-                    height: 70,
-                    child: Row(children: <Widget>[
-                        Expanded(child: 
-                          Text(
-                            _selecteDate == null ? 
-                            'No Date Chosen' : 
-                            'Picked Date: ${DateFormat.yMd().format(_selecteDate)}'
-                          ),
-                        ),
-                        FlatButton(
-                          textColor: Theme.of(context).primaryColor,
-                          child: Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),), 
-                          onPressed: _presentDatePicker,
-                        )
-                      ],
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                     ),
                   ),
-                  RaisedButton(
-                    child: Text('Add Transaction'), 
-                    textColor: Theme.of(context).textTheme.button.color,
-                    color: Theme.of(context).primaryColor,
-                    onPressed: () => {
-                      _submitData()
-                    },
-                  )
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _presentDatePicker,
+                  ),
                 ],
               ),
             ),
-          );
+            RaisedButton(
+              child: Text('Add Transaction'),
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
+              onPressed: _submitData,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
